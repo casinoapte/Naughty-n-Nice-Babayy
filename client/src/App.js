5b43b122858246912c2aef7373299f220d98dd9e
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-12-07 10:18:33 -0700

    Updated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import CreateGroup from "./components/createGroup/CreateGroup";
-import FindGroup from "./components/findGroup/FindGroup"
+import GroupForms from "./components/groupForms/GroupForms"
 import MainPage from "./components/mainPage/MainPage";
 import MainGroupPage from "./pages/main-group-page/MainGroupPage";
 
 function App() {
   return (
-    <Router>
+  <Router>
     <>
-    <MainPage></MainPage>
-    <Route exact path="/" component={CreateGroup}></Route>
-    <Route exact path="/" component={FindGroup}></Route>
+    <MainPage/>
+    <Route exact path="/" component={GroupForms}></Route>
     <Route exact path="/:_id/:name" component={MainGroupPage}></Route>
+    {/* <Route exact path="" component={NotFound}></Route> */}
     </>
   </Router>
   )
